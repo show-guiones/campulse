b98f6d5025f22dea4b8301c64188ed9106f229c4
show-guiones <mikebrandon695@gmail.com>
2026-03-21 02:40:17 -0500

    feat: rewrite /sitemap.xml a /api/sitemap

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
